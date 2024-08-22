52d97ba598fde76ae82ba98f4dc829d7e913ff6a
RyomenDev <aakashvatsh007@gmail.com>
2024-08-22 17:13:33 +0530

    Day_8

---
+++ Day_8/class/src/components/Body.js
@@ -0,0 +1,12 @@
+
+import { Link } from "react-router-dom";
+
+const Body = () => {
+  return (
+    <div className="body">
+      <h1>This is Body</h1>;
+    </div>
+  );
+};
+
+export default Body;
---
+++ Day_8/class/src/components/Header.js
@@ -0,0 +1,61 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  //   let btnName = 'Login';
+
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  // console.log('header render');
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        {/* <img src={LOGO_URL} alt="App Logo" className="logo" /> */}
+        <Link to="/">
+          <img
+            src="https://cdn-icons-png.flaticon.com/128/3655/3655682.png"
+            alt="Logo"
+            className="logo"
+          />
+        </Link>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/" className="links">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="links">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="links">
+              Contact Us
+            </Link>
+          </li>
+          <li>
+            <Link className="links">Cart</Link>
+          </li>
+          <button
+            className="loginBtn"
+            onClick={() => {
+              //   btnName = 'Logout';
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+              console.log(btnNameReact);
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
